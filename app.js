73abaeb1df009e68b9b271f66a725881bdb1f2d7
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-10-28 11:20:21 -0700

    return an array for the quote and author name

--- app.js
+++ app.js
@@ -27,10 +27,13 @@
 //     }
 // }
 
+let para = document.querySelector(".para");
+let writer = document.querySelector(".writer");
 let btn = document.querySelector("#button");
-btn.addEventListener("click", () => {
-    let fac = getquote();
-    console.log(fac);
+btn.addEventListener("click", async () => {
+    let fac = await getquote();
+    para.innerText = fac[0];
+    writer.innerText = fac[1];
 });
 
 async function getquote() {
@@ -38,7 +41,9 @@ async function getquote() {
         let response = await axios.get("https://quote-garden.onrender.com/api/v3/quotes");
         let random_num = Math.floor(Math.random()*5)+1;
         let quote = await response.data.data[random_num].quoteText;
-        return quote;
+        let auth = await response.data.data[random_num].quoteAuthor;
+        let data = [quote,auth];
+        return data;
     } catch (error) {
         return "Sorry, there is an issue in the API right now :(";
     }
