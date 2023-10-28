5c3790b708474fda87709bc4a2b7fd6342249f0f
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-10-28 11:35:57 -0700

    Added responsiveness

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-// let btn = document.querySelector("#button")
-// btn.addEventListener("click", () => {
-//     let fac = getquote();
-//     console.log(fac);
-// });
-
-
-// async function getquote() {
-//     let quote =  await axios.get("https://quote-garden.onrender.com/api/v3/quotes");
-//     return  quote.data.quoteText;
-     
-// }
-
-
-// let btn = document.querySelector("#button");
-// btn.addEventListener("click", () => {
-//     getQuote(); // Call the getQuote function when the button is clicked
-// });
-
-// async function getQuote() {
-//     try {
-//         const response = await axios.get("https://api.example.com/quotes"); // Replace with a valid API endpoint
-//         const quoteData = response.data;
-//         console.log(quoteData);
-//     } catch (error) {
-//         console.error("Error getting the quote:", error);
-//     }
-// }
-
 let para = document.querySelector(".para");
 let writer = document.querySelector(".writer");
 let btn = document.querySelector("#button");
 btn.addEventListener("click", async () => {
     let fac = await getquote();
-    para.innerText = fac[0];
-    writer.innerText = fac[1];
+    para.innerText = `"${fac[0]}"`;
+    writer.innerText = `~ ${fac[1]}`;
 });
 
 async function getquote() {
